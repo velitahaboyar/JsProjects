dac69736ed89e6ec7c0465f9fe33d590795974c9
Veli Taha <67549099+velitahaboyar@users.noreply.github.com>
2024-10-08 19:26:09 +0300

    Random Password Generator

---
+++ Random Password Generator/src/index.js
@@ -0,0 +1,29 @@
+const passwordBox = document.getElementById('password');
+const length = 12;
+
+const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const lowerCase = 'abcdefghijklmnopqrstuvwxyz';
+const numbers = '0123456789';
+const symbols = '!@#$%^&*()_+';
+
+function generatePassword() {
+	let password = '';
+	password += upperCase[Math.floor(Math.random() * upperCase.length)];
+	password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+	password += numbers[Math.floor(Math.random() * numbers.length)];
+	password += symbols[Math.floor(Math.random() * symbols.length)];
+
+	const allCharacters = upperCase + lowerCase + numbers + symbols;
+
+	while (length > password.length) {
+		password += allCharacters[Math.floor(Math.random() * allCharacters.length)];
+	}
+	passwordBox.value = password;
+}
+
+function copyPassword() {
+	passwordBox.select();
+	document.execCommand('copy');
+}
+
+
